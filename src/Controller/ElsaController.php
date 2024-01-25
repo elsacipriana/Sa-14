1f5657692fed61483e64f68f07198b4b13646e8f
Your Name <you@example.com>
2024-01-25 11:53:59 +0100

    messge

--- src/Controller/ElsaController.php
+++ src/Controller/ElsaController.php
@@ -1,34 +1,83 @@
 <?php
 
+
+
 namespace App\Controller;
 
+
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Component\Routing\Annotation\Route;
 
+
+
 class ElsaController extends AbstractController
+
 {
+
     #[Route('/elsa', name: 'app_elsa')]
+
     public function index(): Response
+
     {
+
         return $this->render('elsa/index.html.twig', [
+
             'controller_name' => 'ElsaController',
+
         ]);
+
     }
+
+
 
     #[Route('/home', name: 'app_home')]
+
     public function home()
+
     {
+
         return $this->render('elsa/home.html.twig', [
+
             
+
         ]);
+
     }
 
+
+
     #[Route('/elsa/loisirs', name: 'Loisirs')]
+
     public function loisirs()
+
     {
+
         return $this->render('elsa/loisirs.html.twig', [
+
             
+
         ]);
+
     }
+
+
+
+    #[Route('/elsa/competences', name: 'Competences')]
+
+    public function competences()
+
+    {
+
+        return $this->render('elsa/competences.html.twig', [
+
+            
+
+        ]);
+
+    }
+
 }
